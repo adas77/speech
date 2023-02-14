f367debc28116f1ab7ba8219a4ddf1623bb14214
Adam Pilewski <apilewski01@gmail.com>
2023-02-14 22:15:52 +0100

    speech to text

---
+++ README.md
@@ -0,0 +1,12 @@
+# Download lib
+`pip install -r requirements.txt`
+
+# Optymalizacja przy użyciu GPU
+[Nvidia Cuda](https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=22.04&target_type=deb_local)
+[Nvidia Cuda](https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=22.04&target_type=deb_local)
+
+
+https://www.geeksforgeeks.org/running-python-script-on-gpu/
+
+https://askubuntu.com/questions/1436506/how-to-resolve-unmet-dependencies-error-when-upgrading-depends-nvidia-kernel-c
+
---
+++ audio.py
@@ -0,0 +1,139 @@
+import os
+
+import IPython.display as ipd
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+from keras import backend as K
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.layers import Conv1D, Dense, Dropout, Flatten, Input, MaxPooling1D
+from keras.models import Model, load_model
+from keras.utils import np_utils
+from numba import cuda, jit
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+
+class Audio:
+    def __init__(self, audio_file: str, figsize=(14, 5)):
+        self.audio_file = audio_file
+        self.x, self.sr = librosa.load(audio_file)
+        self.figsize = figsize
+
+    def waveshow(self):
+        plt.figure(figsize=self.figsize)
+        librosa.display.waveshow(self.x, sr=self.sr)
+
+    def spectogram(self, log=False):
+        X = librosa.stft(self.x)
+        Xdb = librosa.amplitude_to_db(abs(X))
+        plt.title("ego0")
+        plt.figure(figsize=self.figsize)
+        librosa.display.specshow(
+            Xdb, sr=self.sr, x_axis='time', y_axis='log' if log else 'hz')
+        plt.colorbar()
+
+    def play(self):
+        ipd.Audio(self.audio_file)
+
+    def write_to_file(self, filename: str):
+        librosa.output.write_wav(filename, self.x, self.sr)
+
+    def mfcc(self):
+        mfcc = librosa.feature.mfcc(x=self.x, sr=self.sr)
+        print(mfcc.shape)
+        plt.figure(figsize=self.figsize)
+        librosa.display.specshow(mfcc, sr=self.sr, x_axis='time')
+        plt.show()
+
+    
+class SpeechToText():
+    def __init__(self, train_audio_path: str, output_model_file: str, sample_rate=8000, figsize=(14, 5)):
+        self.train_audio_path = train_audio_path
+        self.figsize = figsize
+        self.sample_rate = sample_rate
+        self.output_model_file = output_model_file
+
+    @jit(target_backend='cuda')
+    def train(self):
+
+        all_wave = []
+        all_label = []
+        labels = os.listdir(self.train_audio_path)
+        # labels = ["yes", "no"]
+        for label in labels:
+            print(label)
+            waves = [f for f in os.listdir(
+                self.train_audio_path + '/' + label) if f.endswith('.wav')]
+            for wav in waves:
+                samples, sample_rate = librosa.load(
+                    self.train_audio_path + '/' + label + '/' + wav, sr=16000)
+                samples = librosa.resample(
+                    samples, sample_rate, self.sample_rate)
+                if (len(samples) == self.sample_rate):
+                    all_wave.append(samples)
+                    all_label.append(label)
+        #
+        le = LabelEncoder()
+        y = le.fit_transform(all_label)
+        self.classes = list(le.classes_)
+        #
+        y = np_utils.to_categorical(y, num_classes=len(labels))
+        all_waves = np.array(all_wave).reshape(-1, self.sample_rate, 1)
+        # split to  train and test
+        TEST_SIZE = 0.2
+        x_tr, x_val, y_tr, y_val = train_test_split(
+            np.array(all_wave), np.array(y), stratify=y, test_size=TEST_SIZE, random_state=7)
+        # conv layers
+        K.clear_session()
+        inputs = Input(shape=(self.sample_rate, 1))
+        # First Conv1D layer
+        conv = Conv1D(8, 13, padding='valid',
+                      activation='relu', strides=1)(inputs)
+        conv = MaxPooling1D(3)(conv)
+        conv = Dropout(0.3)(conv)
+        # Second Conv1D layer
+        conv = Conv1D(16, 11, padding='valid',
+                      activation='relu', strides=1)(conv)
+        conv = MaxPooling1D(3)(conv)
+        conv = Dropout(0.3)(conv)
+        # Third Conv1D layer
+        conv = Conv1D(32, 9, padding='valid',
+                      activation='relu', strides=1)(conv)
+        conv = MaxPooling1D(3)(conv)
+        conv = Dropout(0.3)(conv)
+        # Fourth Conv1D layer
+        conv = Conv1D(64, 7, padding='valid',
+                      activation='relu', strides=1)(conv)
+        conv = MaxPooling1D(3)(conv)
+        conv = Dropout(0.3)(conv)
+        # Flatten layer
+        conv = Flatten()(conv)
+        # Dense Layer 1
+        conv = Dense(256, activation='relu')(conv)
+        conv = Dropout(0.3)(conv)
+        # Dense Layer 2
+        conv = Dense(128, activation='relu')(conv)
+        conv = Dropout(0.3)(conv)
+        outputs = Dense(len(labels), activation='softmax')(conv)
+        model = Model(inputs, outputs)
+        model.summary()
+
+        #
+        model.compile(loss='categorical_crossentropy',
+                      optimizer='adam', metrics=['accuracy'])
+        es = EarlyStopping(monitor='val_loss', mode='min',
+                           verbose=1, patience=10, min_delta=0.0001)
+        mc = ModelCheckpoint(
+            'best_model.hdf5', monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+        history = model.fit(x_tr, y_tr, epochs=100, callbacks=[
+            es, mc], batch_size=32, validation_data=(x_val, y_val))
+
+        model.save(self.output_model_file)
+
+    def predict(self, audio) -> str:
+        model = load_model(self.output_model_file)
+        prob = model.predict(audio.reshape(1, self.sample_rate, 1))
+        index = np.argmax(prob[0])
+        return self.classes[index]
